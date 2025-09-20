a04f976a91154b599bb79ee4a791009d17bcfacb
username <wecp-dummy@gmail.com>
2025-09-20 09:30:18 +0000

    wecp initial commit

---
+++ frontend/src/app/bank/components/account/account.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-account',
+  templateUrl: './account.component.html'
+})
+export class AccountComponent implements OnInit {
+  accountForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.accountForm = this.fb.group({
+      accountId: [null, [Validators.required, Validators.min(1)]],
+      customerId: [null, Validators.required],
+      balance: [0, [Validators.required, Validators.min(0)]]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.accountForm.valid) {
+      console.log('Account Data:', this.accountForm.value);
+      // Call backend API here
+    } else {
+      this.accountForm.markAllAsTouched();
+    }
+  }
+}
